06206bc1b2fb29001f8f4cd5c4ad30be393f666a
Abdul Rehman Sohail <abdulrehmansohail786@gmail.com>
2022-09-21 15:41:33 +0500

    super u

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from home.views import ListingViewSet
+from home.views import ListingViewSet, SpView
 
 router = DefaultRouter()
 router.register('listing', ListingViewSet, basename='listing')
+router.register('sp', SpView, basename='sp')
 
 urlpatterns = [
     path('home/', include(router.urls)),
